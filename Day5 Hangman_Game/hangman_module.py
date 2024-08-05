c983efcc404b60a7e2ecd77311567bc699e992d3
sami-thedev <95037632+sami-thedev@users.noreply.github.com>
2024-08-05 23:32:31 +0500

    Delete Day5 Hangman_Game directory

--- Day5 Hangman_Game/hangman_module.py
+++
@@ -1,7 +0,0 @@
-word_list = [
-  "apple", "banana", "cherry", "date", "elderberry", "fig", "grape", "honeydew",
-  "kiwi", "lemon", "mango", "nectarine", "orange", "papaya", "quince", "raspberry",
-  "strawberry", "tangerine", "ugli", "vanilla", "watermelon", "xigua", "yellowfruit", "zucchini",
-  "avocado", "blackberry", "cantaloupe", "dragonfruit", "elderflower", "feijoa", "gooseberry",
-  "huckleberry", "jackfruit", "kumquat", "lychee", "mulberry", "nectar", "olive", "plum"
-]
