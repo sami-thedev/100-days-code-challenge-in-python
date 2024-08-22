ecea119d0d143e4ac3e2e8e8b7511c3dbb5f35cf
sami-thedev <95037632+sami-thedev@users.noreply.github.com>
2024-08-22 23:14:21 +0500

    Delete day 1 band name generator.py

--- day 1 band name generator.py
+++
@@ -1,7 +0,0 @@
-print("Welcome to the Band Name Generator.")
-
-country_name = input('What is the name of the city you grew up in? \n')
-
-pet_name = input('What is the name of your pet? \n')
-
-print("Your band name could be = " + country_name + " " + pet_name)
